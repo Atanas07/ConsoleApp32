599cca8f2147f46d2d0590924955d3408034dbc7
Atanas07 <atanassv@abv.bg>
2025-06-11 09:20:12 +0300

    initial commit

---
+++ ConsoleApp31/BattleManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    public class BattleManager
+    {
+        private Hero champion1;
+        private Hero champion2;
+    
+
+        public BattleManager(Hero hero1, Hero hero2)
+        {
+            champion1 = hero1;
+            champion2 = hero2;
+
+            champion1.OnAttack += HandleAttack;
+            champion1.OnDamaged += HandleDamaged;
+            
+           
+            champion2.OnAttack += HandleAttack;
+            champion2.OnDamaged += HandleDamaged;
+       
+        }
+        private void HandleAttack(Hero   attacker, Hero target, int damage)
+        {
+            Console.WriteLine($"Герой {attacker.Name} атакува Герой {target.Name} с {damage} щети!");
+        }
+
+        private void HandleDamaged(Hero champion, int remainingHealth)
+        {
+            Console.WriteLine($"Герой {champion.Name} има {remainingHealth} здраве.");
+        }
+
+
+       
+        public void StartBattle()
+        {
+            Console.WriteLine("Битката започва!");
+            Console.WriteLine($"Герой {champion1.Name} (Здраве: {champion1.Health}, Атака: {champion1.AttackPower})");
+            Console.WriteLine($"срещу");
+            Console.WriteLine($"Герой {champion2.Name} (Здраве: {champion2.Health}, Атака: {champion2.AttackPower})");
+            Console.WriteLine();
+            int turn = 0;
+            while ( !champion1.IsDefeated && !champion2.IsDefeated)
+            {
+                if (turn % 2 == 0)
+                { 
+                    champion1.Attack(champion2);
+                }
+                else
+                {
+              champion2.Attack(champion1);
+                }
+                turn++;
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("GG FF 15");
+        }
+
+    }
+
+}
---
+++ ConsoleApp31/Hero.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    public delegate void AttackEventHandler(Hero attacker, Hero target, int damage);
+    public delegate void DamagedEventHandler(Hero champion, int remainingHealth);
+    public delegate void DefeatedEventHandler(Hero winner, Hero loser);
+    public class Hero
+    {
+        public string Name { get; private set; }
+        public int Health { get; private set; }
+        public int AttackPower { get; private set; }
+        public bool IsDefeated => Health <= 0;
+        public event AttackEventHandler OnAttack;
+        public event DamagedEventHandler OnDamaged;
+        public event DefeatedEventHandler OnDefeated;
+        public Hero(string name, int health, int attackPower)
+        {
+            Name = name;
+            Health = health;
+            AttackPower = attackPower;
+        }
+        public void Attack(Hero target)
+        {
+            if (IsDefeated || target.IsDefeated)
+            {
+                return;
+            }
+               
+            OnAttack?.Invoke(this, target, AttackPower);
+            target.TakeDamage(AttackPower);
+        }
+
+        public void TakeDamage(int amount)
+        {
+            if (IsDefeated)
+            {
+                return;
+            }
+            Health -= amount;
+            
+            OnDamaged?.Invoke(this, Health); 
+            if (Health <= 0)
+            {
+
+                OnDefeated?.Invoke(null, this);
+                Console.WriteLine($"{Name} was killed");
+            }
+        }
+
+
+    }
+}
